f9708726192d0b9459f04c026603e171696b70f4
Marco Di Bella <dibella@rechtsdepesche.de>
2018-03-21 16:01:30 +0100

    Hauptdatei

--- mdb-image-rights/mdb-image-rights.php
+++ mdb-image-rights/mdb-image-rights.php
@@ -1,13 +1,23 @@
 <?php
-/**
- *	Plugin Name:     Bildrechte (mdb-image-rights)
- *	Description:     Implementiert eine Bildrechteverwaltung.
- *	Version:         0.0.1
- *	Author:          Marco Di Bella
- *	Author URI:      https://www.marcodibella.de
- *	Text Domain:     mdb-image-rights
- *	Domain Path:     languages
- */
+/*
+Plugin Name:     Bildrechte (mdb-image-rights)
+Author:          Marco Di Bella
+Author URI:      https://www.marcodibella.de
+Description:     Implementiert eine Bildrechteverwaltung.
+Version:         0.0.1
+Text Domain:     mdb-image-rights
+*/
 
 
- 
+// Wichtige Konstanten
+define( 'PLUGIN_VERSION', '0.0.1' );
+define( 'PLUGIN_DOMAIN', 'mdb-image-rights' );
+
+
+
+function plugin_activation()
+{
+    //do something
+}
+
+register_activation_hook( __FILE__, 'plugin_activation' );
