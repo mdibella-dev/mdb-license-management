51c5b015e403838a2cd36ca630c1919358ff57bb
Marco Di Bella <dibella@rechtsdepesche.de>
2018-03-26 16:09:31 +0200

    Indizierung

--- mdb-lizenzverwaltung/inc/mainpage.php
+++ mdb-lizenzverwaltung/inc/mainpage.php
@@ -25,7 +25,7 @@ function mdb_lv_add_mainpage()
     add_media_page( __( 'Lizenzverwaltung', 'mdb_lv' ),
                     __( 'Lizenzverwaltung', 'mdb_lv' ),
                     'manage_options',
-                    __FILE__,
+                    'mdb_lizenzverwaltung',
                     'mdb_lv_show_mainpage' );
 }
 
@@ -63,8 +63,8 @@ function mdb_lv_show_mainpage()
     endif;
 ?>
 <h2 class="nav-tab-wrapper">
-<a href="?page=mdb-lizenzverwaltung%2Finc%2Fmainpage.php&tab=tab-01" class="nav-tab <?php if( $active_tab == 'tab-01'){ echo 'nav-tab-active'; } ?>"><?php _e( 'Indizierung', 'mdb_lv'); ?></a>
-<a href="?page=mdb-lizenzverwaltung%2Finc%2Fmainpage.php&tab=tab-02" class="nav-tab <?php if( $active_tab == 'tab-02'){ echo 'nav-tab-active'; } ?>"><?php _e( 'Verfügbare Lizenzen', 'mdb_lv'); ?></a>
+<a href="?page=mdb_lizenzverwaltung&tab=tab-01" class="nav-tab <?php if( $active_tab == 'tab-01'){ echo 'nav-tab-active'; } ?>"><?php _e( 'Indizierung', 'mdb_lv'); ?></a>
+<a href="?page=mdb_lizenzverwaltung&tab=tab-02" class="nav-tab <?php if( $active_tab == 'tab-02'){ echo 'nav-tab-active'; } ?>"><?php _e( 'Verfügbare Lizenzen', 'mdb_lv'); ?></a>
 </h2>
 <?php
     switch( $active_tab ) :
@@ -92,7 +92,7 @@ function mdb_lv_show_mainpage()
 function mdb_lv_show_licenses_tab()
 {
 ?>
-<h2><?php _e( 'Verfügbare Lizenzen', 'mdb_lv' )?></h2>
+<h2><?php echo __( 'Verfügbare Lizenzen', 'mdb_lv' )?></h2>
 <?php
     $maintable = new MDB_main_table();
     $maintable->prepare_items();
@@ -110,8 +110,39 @@ function mdb_lv_show_licenses_tab()
 function mdb_lv_show_indexing_tab()
 {
 ?>
-<h2><?php _e( 'Indizierung der Medien', 'mdb_lv' )?></h2>
+<h2><?php echo __( 'Indizierung der Medien', 'mdb_lv' )?></h2>
 <?php
+    global $wpdb;
+           $total = $wpdb->get_var( "SELECT COUNT(ID) FROM $wpdb->posts WHERE (post_type='attachment') AND (post_mime_type LIKE '%%image%%')" );
 
-    indexing();
+    if( $total == 0 ) :
+        echo '<p>';
+        echo __( 'In der Mediathek befinden sich derzeit keine Bilder.', 'mdb_lv' );
+        echo '</p>';
+    else :
+        echo '<p>';
+        echo sprintf( __( 'In der Mediathek befinden sich derzeit %1$s Bilder.', 'mdb_lv' ), $total );
+        echo '<br>';
+        echo __( 'Bevor Sie mit der Erfassung der Bildlizenzen fortfahren, führen Sie bitte einmalig den Indizierungsvorgang durch.', 'mdb_lv' );
+        echo '</p>';
+        echo '<form action="" method="post">';
+        echo '<input name="mdb_lv_index" type="hidden" value="go">';
+        echo '<input type="submit" class="button" value="Starten">';
+        echo '</form>';
+
+        if( $_POST[ 'mdb_lv_index' ] == 'go' ):
+            $data = $wpdb->get_results( "SELECT ID, post_title FROM $wpdb->posts WHERE (post_type='attachment') AND (post_mime_type LIKE '%%image%%')" );
+
+            echo '<p>';
+            foreach( $data as $image ) :
+                echo( sprintf( __( 'Bearbeite Bild mit ID #%1$s: %2$s', 'mdb_lv'), $image->ID, $image->post_title ) );
+                echo '<br>';
+            endforeach;
+            echo '</p>';
+            echo '</p>';
+            echo __( 'Indizierungsvorgang beendet.', 'mdb_lv' );
+            echo '</p>';
+        endif;
+    endif;
+
 }
