ba6bcd5b008c9b66e1fbac1e5a8a3eb82ebbd5d2
mdibella-dev <mdb@marcodibella.de>
2022-03-27 18:35:56 +0200

    Obsolete code removed

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,37 +1,4 @@
 jQuery(function($) {
-/*
-    @todo: Wiederherstellung der Funktionalität für 'normale' Attachments
-
-    function mdb_lv_setFormFields() {
-        val = $( '#mdb-lv-media-state' ).val();
-
-        $( '.compat-field-mdb-lv-media-state' ).css( 'display', 'none' );
-
-        $( '.compat-field-mdb-lv-by-link' ).hide();
-        $( '.compat-field-mdb-lv-by-name' ).hide();
-        $( '.compat-field-mdb-lv-license-guid' ).hide();
-        $( '.compat-field-mdb-lv-media-link' ).hide();
-
-        if( val > 0 ) {
-            $( '.compat-field-mdb-lv-media-link' ).show();
-        }
-
-        if( val > 1 ) {
-            $( '.compat-field-mdb-lv-by-link' ).show();
-            $( '.compat-field-mdb-lv-by-name' ).show();
-        }
-
-        if( val > 2 ) {
-            $( '.compat-field-mdb-lv-license-guid' ).show();
-        }
-    }
-*/
-    $(document).ready( function() {
-    //    mdb_lv_setFormFields();
-    } );
-
-
-
 
     /**
      * Für MediaGrid
