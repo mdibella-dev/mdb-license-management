713685bb11a86f1a205cf480126abd3b62bfe652
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2023-02-06 11:47:12 +0100

    Add plugin functions and the ability for translations.

--- includes/setup.php
+++ includes/setup.php
@@ -15,9 +15,24 @@ defined( 'ABSPATH' ) or exit;
 
 
 
+/**
+ * The init function for the plugin.
+ *
+ * @since 0.0.3
+ */
+
+function plugin_init()
+{
+    // Load text domain, use relative path to the plugin's language folder
+    load_plugin_textdomain( 'mdb-license-management', false, plugin_basename( PLUGIN_DIR ) . '/languages' );
+}
+
+add_action( 'init', __NAMESPACE__ . '\plugin_init', 9 );
+
+
 
 /**
- * Zentrale Aktivierungsfunktion für das Plugin
+ * The activation function for the plugin.
  *
  * @since 0.0.1
  */
@@ -103,3 +118,35 @@ function plugin_activation()
 }
 
 register_activation_hook( __FILE__, __NAMESPACE__ . '\plugin_activation' );
+
+
+
+/**
+ * The deactivation function for the plugin.
+ *
+ * @since 0.0.3
+ */
+
+function plugin_deactivation()
+{
+    // Do something!
+}
+
+register_deactivation_hook( __FILE__, __NAMESPACE__ . '\plugin_deactivation' );
+
+
+
+/**
+ * The uninstall function for the plugin.
+ *
+ * @since 0.0.3
+ */
+
+function plugin_uninstall()
+{
+    // Do something!
+    // Delete options!
+    // Delete custom tables!
+}
+
+register_uninstall_hook( __FILE__, __NAMESPACE__ . '\plugin_uninstall' );
