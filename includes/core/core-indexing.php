81f14ab6452e18bd62180e63a83dcef4adbafc9b
Marco Di Bella <mdb@marcodibella.de>
2023-02-06 18:24:36 +0100

    Simplification of the function and improvement of the documentation.

--- includes/core/core-indexing.php
+++ includes/core/core-indexing.php
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) or exit;
 
 
 /**
- * Reads out a medium (attachment) and prepares an entry for the media table.
+ * Reads out a medium (attachment) and prepares a (largely empty) entry for the media table.
  *
  * @since 0.0.1
  *
@@ -27,24 +27,14 @@ function mdb_lv_indexing( $id )
 {
     global $wpdb;
 
-    $media_link   = '';
-    $media_state  = MEDIA_STATE_UNKNOWN;
-    $by_name      = '';
-    $license_guid = '';
-
-
-    /**
-     * Fill table
-     */
-
     $table_name   = $wpdb->prefix . TABLE_MEDIA;
     $table_format = array( '%d', '%s', '%s', '%s', '%s', '%s' );
     $table_data   = array(
         'media_id'     => $id,
-        'media_link'   => $media_link,
-        'media_state'  => $media_state,
-        'license_guid' => $license_guid,
-        'by_name'      => $by_name,
+        'media_link'   => '',
+        'media_state'  => MEDIA_STATE_UNKNOWN,
+        'license_guid' => '',
+        'by_name'      => '',
         'by_link'      => ''
     );
 
