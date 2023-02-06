08f53c3cb2460f6f37910b08deacb61ecc7c3eb2
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2023-02-06 11:30:59 +0100

    Add plugin constants.

--- plugin.php
+++ plugin.php
@@ -20,6 +20,10 @@ defined( 'ABSPATH' ) or exit;
 
 /** Variables and definitions */
 
+define( __NAMESPACE__ . '\PLUGIN_VERSION', '0.0.2' );
+define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
 define( 'LICENSE_GUID_CC0', '0001' );
 define( 'LICENSE_GUID_DREAMSTIME_RF', 'L054' );
 
@@ -29,12 +33,13 @@ define( 'MEDIA_STATE_SIMPLE_CREDIT', 2 );
 define( 'MEDIA_STATE_LICENSED', 3 );
 
 
+
 /** Include files */
 
-require_once( __DIR__ . '/includes/classes/class-main-table.php' );
-require_once( __DIR__ . '/includes/core/core-indexing.php' );
-require_once( __DIR__ . '/includes/core/core-media-record.php' );
-require_once( __DIR__ . '/includes/core/core-license.php' );
-require_once( __DIR__ . '/includes/media-library-enhancement.php' );
-require_once( __DIR__ . '/includes/mainpage.php' );
-require_once( __DIR__ . '/includes/setup.php' );
+require_once( PLUGIN_DIR . '/includes/classes/class-main-table.php' );
+require_once( PLUGIN_DIR . '/includes/core/core-indexing.php' );
+require_once( PLUGIN_DIR . '/includes/core/core-media-record.php' );
+require_once( PLUGIN_DIR . '/includes/core/core-license.php' );
+require_once( PLUGIN_DIR . '/includes/media-library-enhancement.php' );
+require_once( PLUGIN_DIR . '/includes/mainpage.php' );
+require_once( PLUGIN_DIR . '/includes/setup.php' );
