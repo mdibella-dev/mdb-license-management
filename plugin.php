2baee76e9cd4cfefa68775217d2ef3d4f96b4cd8
Marco Di Bella <mdb@marcodibella.de>
2023-08-05 17:48:49 +0200

    Fix include paths

--- plugin.php
+++ plugin.php
@@ -28,20 +28,19 @@ define( __NAMESPACE__ . '\PLUGIN_VERSION', '0.0.4' );
 define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-define( 'MEDIA_STATE_UNKNOWN', 0 );
-define( 'MEDIA_STATE_NO_CREDIT', 1 );
-define( 'MEDIA_STATE_SIMPLE_CREDIT', 2 );
-define( 'MEDIA_STATE_LICENSED', 3 );
 
 const TABLE_LICENSES = 'mdb_lv_licenses';
 const TABLE_MEDIA    = 'mdb_lv_media';
 
 
-
 /** Include files */
-require_once( PLUGIN_DIR . 'includes/licenses.php' );
-require_once( PLUGIN_DIR . 'includes/core/index.php' );
+
+require_once( PLUGIN_DIR . 'includes/definitions/media-states.php' );
+require_once( PLUGIN_DIR . 'includes/definitions/licenses.php' );
+
+require_once( PLUGIN_DIR . 'includes/classes/class-media-record.php' );
 
 require_once( PLUGIN_DIR . 'includes/theme-integration.php' );
+require_once( PLUGIN_DIR . 'includes/media-library.php' );
 require_once( PLUGIN_DIR . 'includes/backend.php' );
 require_once( PLUGIN_DIR . 'includes/setup.php' );
