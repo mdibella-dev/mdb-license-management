ae7366b67aa29bcd23113e8db19d8ef524ced992
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2025-09-23 15:01:16 +0200

    Remove edit link from taxonomy table; do some code styling

--- includes/taxonomies/taxonomy-media-license.php
+++ includes/taxonomies/taxonomy-media-license.php
@@ -81,21 +81,21 @@ function register() {
 function setup() {
     $preset = include "licenses.php";
 
-    foreach ($preset['licenses'] as $license) {
+    foreach ( $preset['licenses'] as $license ) {
 
         wp_insert_term(
             $license['term'],
             'media_license',
             [
                 'description' => $license['description'],
-                'slug' => $license['slug']
+                'slug'        => $license['slug']
             ]
         );
 
-        $result = term_exists($license['term'], 'media_license');
+        $result = term_exists( $license['term'], 'media_license' );
 
-        if ((0 !== $result) and (NULL !== $result)) {
-            update_term_meta($result['term_id'], LICENSE_METAKEY_LINK, $license['link']);
+        if ( (0 !== $result) and (NULL !== $result) ) {
+            update_term_meta( $result['term_id'], LICENSE_METAKEY_LINK, $license['link'] );
         }
     }
 }
@@ -114,3 +114,31 @@ function init() {
 }
 
 add_action( 'init', __NAMESPACE__ . '\init' );
+
+
+
+/**
+ * Filters the edit link for a term.
+ *
+ * This filter removes any edit link of a given term of taxonomy media_license.
+ *
+ * @see https://developer.wordpress.org/reference/functions/get_edit_term_link/
+ *
+ * @since 2.0.0
+ *
+ * @param string $location    The edit link
+ * @param int    $term_id     Term ID
+ * @param string $taxonomy    Taxonomy name
+ * @param string $object_type The object type
+ */
+
+function media_license_get_edit_term_link( $location, $term_id, $taxonomy, $object_type ) {
+
+    if ( 'media_license' == $taxonomy ) {
+        $location = '';
+    }
+
+    return $location;
+}
+
+add_filter( 'get_edit_term_link', __NAMESPACE__ . '\media_license_get_edit_term_link', 10, 4 );
