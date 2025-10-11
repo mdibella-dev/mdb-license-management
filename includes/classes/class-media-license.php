25e270c078319917e6bb4512e6616350ac5bb98d
Marco Di Bella <m_dibella@icloud.com>
2025-10-11 17:08:48 +0200

    Finalize constructor

--- includes/classes/class-media-license.php
+++ includes/classes/class-media-license.php
@@ -70,18 +70,24 @@ class Media_License {
     /**
      * Constructor
      *
-     * @param string $license_guid The license
+     * @param string $license_guid The license identifier
      */
 
     public function __construct( $license_guid ) {
 
-        // check if license_guid exist
-        // what happens if not?
+        global $wpdb;
 
-        $this->license_guid = $license_guid;
+        $results = $wpdb->get_results(
+            "SELECT * FROM {$wpdb->prefix}mdb_lm_licenses WHERE license_guid='{$license_guid}'",
+            'ARRAY_A'
+        );
 
-        if ( false === $this->get_table_record() ) {
-            // what now?
+        if ( null !== $results ) {
+            $this->license_guid = $results[0]['license_guid'];
+            $this->license_name = $results[0]['license_name'];
+            $this->license_description = $results[0]['license_description'];
+            $this->license_url = $results[0]['license_url'];
+            $this->media_count = $results[0]['media_count'];
         }
     }
 
@@ -138,75 +144,6 @@ class Media_License {
 
     public function get_media_count() {
         return $this->media_count;
-    }
-
-
-
-    /**
-     * Gets the corresponding in the license table and synchronizes the classes' variables
-     *
-     * @return bool true on success, otherwise false
-     */
-
-    private function get_table_record() {
-        global $wpdb;
-
-        $result = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}mdb_lm_licenses WHERE license_id={$this->license_guid}", 'ARRAY_A' );
-
-        if ( null == $result ) {
-            return false;
-        }
-
-        $this->license_name        = $result[0]['license_name'];
-        $this->license_description = $result[0]['license_description'];
-        $this->license_url         = $result[0]['license_url'];
-        $this->media_count         = $result[0]['media_count'];
-
-        return true;
-    }
-
-
-    /**
-     * Updates the corresponding record in the license table.
-     * // If there is no record it creates a new record with the current settings.
-     *
-     * @see Media_Credit::__construct()
-     */
-
-
-     // needs to be rewritten!!!
-    public function update_table_record() {
-        global $wpdb;
-
-        $table_name   = $wpdb->prefix . 'mdb_lm_licenses';
-        $table_data   = [
-            'media_id'         => $this->media_id,
-            'media_source_url' => $this->media_source_url,
-            'license_guid'     => $this->license_guid,
-            'creator_credit'   => $this->creator_credit,
-            'creator_url'      => $this->creator_url
-        ];
-        $table_format = [
-            '%d',
-            '%s',
-            '%s',
-            '%s',
-            '%s'
-        ];
-
-        if ( false === $this->get_table_record() ) {
-            $wpdb->insert(
-                $table_name,
-                $table_data,
-                $table_format
-            );
-        } else {
-            $wpdb->replace(
-                $table_name,
-                $table_data,
-                $table_format
-            );
-        }
     }
 
 }
