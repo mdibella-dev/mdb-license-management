6a0a214a8cfbf068ed52cee8ab83b2907a455d9a
Marco Di Bella <m_dibella@icloud.com>
2025-10-11 18:00:56 +0200

    Update attachment.php

--- includes/media-library/attachment.php
+++ includes/media-library/attachment.php
@@ -30,61 +30,70 @@ defined( 'ABSPATH' ) or exit;
 
 function add_attachment_fields( $form_fields, $post ) {
 
-    $credit = new Media_Credit( $post->ID );
+    global $wpdb;
+           $credit = new Media_Credit( $post->ID );
 
 
     /** Field 1 - listing of available licenses */
 
-    $html  = "<select id='license-guid' name='attachments[{$post->ID}][license-guid]'>";
-    $html .= sprintf(
-        '<option value="%1$s" disabled %3$s>%2$s</option>',
-        '0',
-        __( '--- please select ---', 'mdb-license-management' ),
-        ( '' == $credit->get_license_guid() )? '' : 'selected'
+    $results = $wpdb->get_results(
+        "SELECT license_guid, license_name FROM {$wpdb->prefix}mdb_lm_licenses",
+        'ARRAY_A'
     );
-/*
-    foreach( LICENSES as $license_guid => $license ) {
-        $html .= sprintf(
-            '<option value="%1$s" %3$s>%2$s</option>',
-            $license_guid,
-            $license['license_term'],
-            ( $license_guid == $record->get_license_guid() )? 'selected' : ''
-        );
-    }
-*/
-    $html .= '</select>';
 
-    $form_fields['mdb-license-guid'] = [
-        'label' => __( 'License', 'mdb-license-management' ),
-        'input' => 'html',
-        'html'  => $html,
-    ];
+    if ( null !== $results ) {
+
+        $html  = "<select id='mdb-lm-license-guid' name='attachments[{$post->ID}][mdb-lm-license-guid]'>";
+        $html .= sprintf(
+            '<option value="%1$s" disabled %3$s>%2$s</option>',
+            '0',
+            __( '--- please select ---', 'mdb-license-management' ),
+            ( '' == $credit->get_license_guid() )? '' : 'selected'
+        );
+
+        foreach ( $results as $result ) {
+            $html .= sprintf(
+                '<option value="%1$s" %3$s>%2$s</option>',
+                $result['license_guid'],
+                $result['license_name'],
+                ( $result['license_guid'] == $credit->get_license_guid() )? 'selected' : ''
+            );
+        }
+
+        $html .= '</select>';
+
+        $form_fields['mdb-license-guid'] = [
+            'label' => __( 'License', 'mdb-license-management' ),
+            'input' => 'html',
+            'html'  => $html,
+        ];
+    }
 
 
     /** Field 2 - naming of the creator */
 
-    $form_fields['creator-credit'] = [
+    $form_fields['mdb-lm-creator-credit'] = [
         'label' => __( 'Naming of the creator', 'mdb-license-management' ),
         'input' => 'html',
-        'html'  => "<input type='text' size='128' class='widefat' value='" . esc_html( $credit->get_creator_credit() ) . "' name='attachments[{$post->ID}][creator-credit]'>",
+        'html'  => "<input type='text' size='128' class='widefat' value='" . esc_html( $credit->get_creator_credit() ) . "' name='attachments[{$post->ID}][mdb-lm-creator-credit]'>",
     ];
 
 
     /** Field 3 - link to the creator's website (if required) */
 
-    $form_fields['creator-url'] = [
+    $form_fields['mdb-lm-creator-url'] = [
         'label' => __( 'Link to the creator', 'mdb-license-management' ),
         'input' => 'html',
-        'html'  => "<input type='url' size='128' class='widefat' value='" . esc_url( $credit->get_creator_url() ) . "' name='attachments[{$post->ID}][creator-url]'>",
+        'html'  => "<input type='url' size='128' class='widefat' value='" . esc_url( $credit->get_creator_url() ) . "' name='attachments[{$post->ID}][mdb-lm-creator-url]'>",
     ];
 
 
     /** Field 4 - link to the original image for your own documentation */
 
-    $form_fields[ 'media-source-url' ] = [
+    $form_fields[ 'mdb-lm-media-source-url' ] = [
         'label' => __( 'Link to original file', 'mdb-license-management' ),
         'input' => 'html',
-        'html'  => "<input type='url' size='128' class='widefat' value='" . esc_url( $credit->get_media_source_url() ) . "' name='attachments[{$post->ID}][media-source-url]'>",
+        'html'  => "<input type='url' size='128' class='widefat' value='" . esc_url( $credit->get_media_source_url() ) . "' name='attachments[{$post->ID}][mdb-lm-media-source-url]'>",
     ];
 
     return $form_fields;
@@ -108,10 +117,10 @@ add_filter( 'attachment_fields_to_edit', __NAMESPACE__ . '\add_attachment_fields
 function save_attachment_fields( $post, $attachment ) {
     $credit = new Media_Credit( $post['ID'] );
 
-    $credit->set_media_source_url( sanitize_url( $attachment['mdb-lv-media-link'] ) );
-    $credit->set_license_guid( $attachment['mdb-lv-license-guid'] );
-    $credit->set_creator_credit( sanitize_text_field( $attachment['mdb-lv-by-name'] ) );
-    $credit->set_creator_url( sanitize_url( $attachment['mdb-lv-by-link'] ) );
+    $credit->set_media_source_url( sanitize_url( $attachment['mdb-lm-media-source-url'] ) );
+    $credit->set_license_guid( $attachment['mdb-lm-license-guid'] );
+    $credit->set_creator_credit( sanitize_text_field( $attachment['mdb-lm-creator-credit'] ) );
+    $credit->set_creator_url( sanitize_url( $attachment['mdb-lm-creator-url'] ) );
 
     $credit->update_table_record();
 
