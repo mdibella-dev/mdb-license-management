1c289e53a7cadd5521ab3df888217a6558aa8d71
Marco Di Bella <m_dibella@icloud.com>
2025-10-11 16:39:05 +0200

    Remove media_states

--- plugin.php
+++ plugin.php
@@ -32,8 +32,6 @@ define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 /** Include files */
 
-require_once PLUGIN_DIR . 'includes/definitions/media-states.php';
-
 require_once PLUGIN_DIR . 'includes/classes/index.php';
 
 require_once PLUGIN_DIR . 'includes/media-library/media-list-table.php';
