466f689c5bafb75fc26c2224e6f96e33bc4ee235
Marco Di Bella <m_dibella@icloud.com>
2025-10-19 20:19:28 +0200

    Remove get_license()

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -9,47 +9,12 @@
 namespace mdb_license_management\theme_integration;
 
 use mdb_license_management\classes\Media_Credit;
-use mdb_license_management\classes\Media_License;
 
 
 
 /** Prevent direct access */
 
 defined( 'ABSPATH' ) or exit;
-
-
-
-/**
- * Names the license of a media (with link if applicable).
- *
- * @since 0.0.1
- *
- * @param int $id The media attachment ID
- *
- * @return string Output of the license details.
- */
-
-function get_license( $id ) {
-    $output = '';
-    $credit = new Media_Credit( $id );
-
-/*
-    if ( ! empty( $license_guid ) and ( true == array_key_exists( $license_guid, LICENSES ) ) ) {
-        if ( ! empty( LICENSES[$license_guid]['license_url'] ) ) {
-            $output = sprintf(
-                '<a href="%1$s" target="_blank" rel="noopener" name="%2$s">[%2$s]</a>',
-                LICENSES[$license_guid]['license_url'],
-                LICENSES[$license_guid]['license_term']
-            );
-        } else {
-            $output = sprintf(
-                '[%1$s]',
-                LICENSES[$license_guid]['license_term']
-            );
-        }
-    }*/
-    return $output;
-}
 
 
 
@@ -64,6 +29,7 @@ function get_license( $id ) {
  */
 
 function get_byline( $id ) {
+
     $credit = new Media_Credit( $id );
 
     return $credit->get_creator_credit();
