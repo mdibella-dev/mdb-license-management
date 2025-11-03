50c07cb831cedf8a83f75645a4329f3a6013b217
Marco Di Bella <m_dibella@icloud.com>
2025-11-03 20:17:58 +0100

    Update class-media-license-list.php

--- includes/classes/class-media-license-list.php
+++ includes/classes/class-media-license-list.php
@@ -25,7 +25,7 @@ defined( 'ABSPATH' ) or exit;
  * @since 2.1.0
  */
 
-class Media_License_List extends \wordpress_helper\classes\Admin_Taxonomy_List {
+class Media_License_List extends \wordpress_helper\Admin_Taxonomy_List {
 
     /**
      * The taxonomy.
